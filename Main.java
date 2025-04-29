23fe91bde002e6120c3882a32ceb718586aa61bb
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:21:17 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Main.java
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+        int choice;
+
+        do {
+            System.out.println("\nMenu:\n1. Add Student\n2. Display Students\n3. Search by PRN\n4. Update Student\n5. Delete Student\n6. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scan.nextInt();
+            scan.nextLine();
+
